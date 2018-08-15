68754ed52e273f01426279df28b809af049708d6
Bijon Miles <bijonmiles01@gmail.com>
2018-08-15 00:41:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // add solution here
+
+var empty = [];
+
+function theBeatlesPlay(empty, instrument) {
+  for (var count = 0; count<=empty.length; count++){
+    
+    return(empty[count] + " plays " + instrument[count])
+  }
+}
+
+theBeatlesPlay(musicians, instrument)
+
+var array = [];
+
+function johnLennonFacts(array){
+  var num = 0;
+  while(num<array.length){
+    return(array[num] + "! ")
+    num++;
+  }
+}
+
+johnLennonFacts("I love the Beatles")
